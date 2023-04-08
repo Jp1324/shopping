11556da569eb85e54f482aa1fdde260cafd39b1e
unknown <mishaln3@gmail.com>
2023-04-08 22:45:16 +0530

    second commit

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -11,12 +11,41 @@ import { Component } from '@angular/core';
 export class LoginpageComponent {
   phoneNumber: any;
   otp: any;
+  otpToCompare = "1234" //this is the variable for getting the otp from service.
+  correctFormat = false;
+  otpValidation = false;
+  
   getOTP() {
-    const getPhoneNumber = this.phoneNumber;
-    console.log(getPhoneNumber);
-  }
+    let getPhoneNumber = this.phoneNumber;
+    console.log(getPhoneNumber); 
+    // getPhoneNumber = getPhoneNumber.replace(/[^0-9+#]/g, "");
+
+    // Validates only the length 
+    if (getPhoneNumber.length >= 10) {
+      this.correctFormat = getPhoneNumber;
+      return true;
+    }else{
+      this.correctFormat = false;
+    }
+    return false;
+  } 
 
   checkOTP() {
+   let otpfromUI = this.otp;
+   console.log(otpfromUI); 
    
+   // Compares the Otp from UI and from the service 
+   if(otpfromUI == this.otpToCompare){
+      //Navigation to home/product list page
+      console.log("correct");
+      return true;
+   }else if(otpfromUI == null){
+    console.log("the otp field is empty")
+    this.otpValidation = false;
+   }else{
+    this.otpValidation == false;
+    return false;
+   }
+   return false;
   }
 }
